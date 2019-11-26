bc444da5f8c902e3cfd1f8a1316432dbc31c35d3
MarvinMartin24 <mmartin.mmarvin@gmail.com>
2019-11-26 15:40:33 +0100

    test size

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 import base64_encode from encoder.base64
+import ceil from math
 import unittest
 
 allow_character = set('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/+=')
@@ -9,3 +10,9 @@ def test_base64_encode_character():
     encode = base64_encode("DevOps")
     for char in encode:
         tc.assertIn(char, allow_character)
+
+def test_base64_encode_size():
+    str = "DevOps"
+    req_base64_size = math.ceil(len(str) / 3) * 4
+    encode = base64_encode(str)
+    tc.assertEqual(len(encode), req_base64_size)
